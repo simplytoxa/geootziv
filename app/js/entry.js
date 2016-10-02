2e694cc9b4285521ecee332cdbdc755192beee38
Anton <toxa.simply@google.com>
2016-10-03 00:33:11 +0300

    Some updates

--- app/js/entry.js
+++ app/js/entry.js
@@ -11,7 +11,6 @@ new Promise(res => document.readyState === 'complete' ? res() : window.onload = 
   ymaps.ready(() => {
     let latitude = location.coords.latitude,
         longitude = location.coords.longitude,
-        geoObjects = [],
         // Создание экземпляра карты и его привязка к контейнеру с
         // заданным id ("map").
         myMap = new ymaps.Map('map', {
@@ -29,9 +28,39 @@ new Promise(res => document.readyState === 'complete' ? res() : window.onload = 
         });
 
     // Handling click events and getting coords
+    map.addEventListener('click', e => {
+      const x = e.clientX,
+          y = e.clientY,
+          popup = document.querySelector('#popup');
+      
+      // TODO: Finish Edges
+      
+      if (x + popup.offsetWidth < screen.width && y + popup.offsetHeight < screen.height) {
+        popup.style.left = x + 'px';
+        popup.style.top = y + 'px';
+      } else if(x + popup.offsetWidth > screen.width && y + popup.offsetHeight < screen.height) {
+        popup.style.left = x - ((x + popup.offsetWidth) - screen.width) - 10 + 'px';
+        popup.style.top = y + 5 + 'px';
+      } else if(x + popup.offsetWidth < screen.width && y + popup.offsetHeight > screen.height) {
+        popup.style.left = x + 5 + 'px';
+        popup.style.top = y - ((y + popup.offsetHeight) - screen.availHeight ) + 'px';
+      } else {
+        // popup.style.left = x - popup.offsetWidth + 'px';
+        // popup.style.top = y - popup.offsetHeight + 'px';
+
+        popup.style.left = x - ((x + popup.offsetWidth) - screen.width) + 'px';
+        popup.style.top = (y - ((y + popup.offsetHeight) - screen.height)) + 'px';
+      }
+      
+
+
+      popup.style.opacity = '1';
+    });
+    
     myMap.events.add('click', e => {
       let coords = e.get('coords'),
           points = [];
+      
 
       points.push(coords);
 
